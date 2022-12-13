a7cbc607d4dd0e7febfa0e3e942c4e2109d31bc7
Alex Parkhomenko <alex.wquels@gmail.com>
2022-12-13 20:08:44 +0200

    Implement JS script for collapsible block

--- simplexblock/static/js/src/simplexblock.js
+++ simplexblock/static/js/src/simplexblock.js
@@ -1,22 +1,15 @@
 /* Javascript for SimpleXBlock. */
-function SimpleXBlock(runtime, element) {
+var coll = document.getElementsByClassName("simplexblock");
+var i;
 
-    function updateCount(result) {
-        $('.count', element).text(result.count);
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
     }
-
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
-
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
-        });
-    });
-
-    $(function ($) {
-        /* Here's where you'd do things on page load. */
-    });
+  });
 }
