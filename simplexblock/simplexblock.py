a83b6e00f401f89486893fa65811ea3cc6bb516b
Alex Parkhomenko <alex.wquels@gmail.com>
2022-12-13 20:07:03 +0200

    Implement SimpleXblock with editable fields

--- simplexblock/simplexblock.py
+++ simplexblock/simplexblock.py
@@ -1,70 +1,59 @@
-"""TO-DO: Write a description of what this XBlock is."""
-
 import pkg_resources
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
-from xblock.fields import Integer, Scope
+from xblock.fields import Scope, String
+from xblockutils.resources import ResourceLoader
+from xblockutils.studio_editable import StudioEditableXBlockMixin
 
 
-class SimpleXBlock(XBlock):
-    """
-    TO-DO: document what your XBlock does.
-    """
-
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
-
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
+class SimpleXBlock(XBlock, StudioEditableXBlockMixin):
+    title = String(
+        default="Title",
+        help="editable title block",
     )
+    content = String(
+        default="Content",
+        scope=Scope.content,
+        help="editable content block",
+    )
+
+    editable_fields = ("title", "content")
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
-        """
-        The primary view of the SimpleXBlock, shown to students
-        when viewing courses.
-        """
-        html = self.resource_string("static/html/simplexblock.html")
-        frag = Fragment(html.format(self=self))
+        loader = ResourceLoader("simplexblock")
+        data = dict(title=self.title, content=self.content)
+        template = loader.render_django_template(
+            "static/html/simplexblock.html", context=data
+        )
+        frag = Fragment(template)
         frag.add_css(self.resource_string("static/css/simplexblock.css"))
-        frag.add_javascript(self.resource_string("static/js/src/simplexblock.js"))
-        frag.initialize_js('SimpleXBlock')
+        frag.add_javascript(
+            self.resource_string("static/js/src/simplexblock.js")
+        )
+        frag.initialize_js("SimpleXBlock")
         return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
-
-        self.count += 1
-        return {"count": self.count}
-
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
         return [
-            ("SimpleXBlock",
-             """<simplexblock/>
-             """),
-            ("Multiple SimpleXBlock",
-             """<vertical_demo>
+            (
+                "SimpleXBlock",
+                """<simplexblock/>
+             """,
+            ),
+            (
+                "Multiple SimpleXBlock",
+                """<vertical_demo>
                 <simplexblock/>
                 <simplexblock/>
                 <simplexblock/>
                 </vertical_demo>
-             """),
+             """,
+            ),
         ]
